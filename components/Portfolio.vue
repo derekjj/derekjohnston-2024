d847b2fd1f1e05608d05a7941268521aadb7d798
Derek J <derek@derekjohnston.ca>
2024-02-15 21:38:25 -0500

    yarn lintfix

---
+++ components/Portfolio.vue
@@ -0,0 +1,414 @@
+<template>
+	<section id="portfolio" class="section bg-custom-gray">
+		<div class="container">
+			<h1 class="mb-5"><span class="text-danger">My</span> Portfolio</h1>
+			<div class="portfolio">
+				<div class="filters">
+					<a
+						href="javascript:void(0)"
+						data-filter=".new"
+						class="active"
+						>New</a
+					>
+					<a
+						href="javascript:void(0)"
+						data-filter=".advertising"
+						class=""
+						>Advertising</a
+					>
+					<a
+						href="javascript:void(0)"
+						data-filter=".branding"
+						class=""
+						>Branding</a
+					>
+					<a href="javascript:void(0)" data-filter=".web" class=""
+						>Web</a
+					>
+				</div>
+				<div
+					class="portfolio-container"
+					style="position: relative; height: 816.874px"
+				>
+					<div
+						class="col-md-6 col-lg-4 web new"
+						style="position: absolute; left: 0px; top: 0px"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/web-1.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/web-1.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">WEB</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 web new"
+						style="position: absolute; left: 369.984px; top: 0px"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/web-2.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/web-2.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">WEB</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 advertising new"
+						style="position: absolute; left: 739.968px; top: 0px"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/advertising-2.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/advertising-2.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">ADVERSTISING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 web"
+						style="
+							position: absolute;
+							left: 739.968px;
+							top: 0px;
+							display: none;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/web-4.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/web-4.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">WEB</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 advertising"
+						style="
+							position: absolute;
+							left: 369.984px;
+							top: 0px;
+							display: none;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/advertising-1.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/advertising-1.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">ADVERSITING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 web new"
+						style="
+							position: absolute;
+							left: 739.968px;
+							top: 267.312px;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/web-3.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/web-3.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">WEB</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 advertising new"
+						style="
+							position: absolute;
+							left: 369.984px;
+							top: 296.25px;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/advertising-3.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/advertising-3.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">ADVERSITING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 advertising new"
+						style="
+							position: absolute;
+							left: 739.968px;
+							top: 533.328px;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/advertising-4.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/advertising-4.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">ADVERTISING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 branding new"
+						style="position: absolute; left: 0px; top: 545.438px"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/branding-1.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/branding-1.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">BRANDING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 branding"
+						style="
+							position: absolute;
+							left: 369.984px;
+							top: 0px;
+							display: none;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/branding-2.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/branding-2.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">BRANDING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 branding new"
+						style="
+							position: absolute;
+							left: 369.984px;
+							top: 563.562px;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/branding-3.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/branding-3.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">BRANDING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 branding"
+						style="
+							position: absolute;
+							left: 369.984px;
+							top: 195.922px;
+							display: none;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/branding-4.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/branding-4.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">BRANDING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-md-6 col-lg-4 branding"
+						style="
+							position: absolute;
+							left: 739.968px;
+							top: 253.312px;
+							display: none;
+						"
+					>
+						<div class="portfolio-item">
+							<img
+								src="assets/imgs/branding-5.jpg"
+								class="img-fluid"
+								alt="website template image"
+							/>
+							<div class="content-holder">
+								<a
+									class="img-popup"
+									href="assets/imgs/branding-5.jpg"
+								></a>
+								<div class="text-holder">
+									<h6 class="title">BRANDING</h6>
+									<p class="subtitle">
+										Expedita corporis doloremque velit in
+										totam!
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+	name: 'IndexPage',
+	data() {
+		return {}
+	},
+})
+</script>
