cea81b6a8566c244f3444fed4b64b7498f9e1584
Derek J <derek@derekjohnston.ca>
2024-02-15 21:09:40 -0500

    contact and google map

---
+++ components/Contact/Index.vue
@@ -0,0 +1,106 @@
+<template lang="pug">
+.row.contact(id="contact")
+    .map 
+        iframe(
+            width="100%"
+            height="100%"
+            style="border: 0"
+            loading="lazy"
+            allowfullscreen
+            referrerpolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDpRStcC8FVyAC3QE43Yd4q3m_ucJaQxh8&q=Durham+Region,Ontario")
+    .container
+        .row 
+            .col-lg-8 
+                .contact-form-card 
+                    h4.contact-title
+                        | Send a message
+                    form(action="https://formspree.io/f/derek@derekjohnston.ca" method="post")
+                        .form-group
+                            input.form-control(type="text" id="name" name="name" placeholder="Name *" required)
+                        .form-group 
+                            input.form-control(type="email" id="email" name="email" placeholder="Email *" required)
+                        .form-group 
+                            input.form-control(type="text" id="subject" name="subject" placeholder="Subject *" required)
+                        .form-group
+                            textarea.form-control(placeholder="Message *" id="message" name="message" rows="7" required)
+                        .form-group
+                            button.btn.btn-primary(type="submit") 
+                                | Send Message 
+            .col-lg-4
+                .contact-info-card 
+                    h4.contact-title
+                        | Get in touch 
+                    Info(:location="false")
+</template>
+<script>
+import Vue from 'vue'
+import Info from '~/components/Contact/Info'
+
+export default Vue.extend({
+	name: 'IndexPage',
+	components: {
+		Info,
+	},
+	head() {
+		const key = process.env.TOKEN_GOOGLE
+		return (
+			!window.google && {
+				script: [
+					{
+						hid: 'maps-googleapis',
+						src: `https://maps.googleapis.com/maps/api/js?libraries=places&key=${key}`,
+						defer: true,
+						callback: this.googleAutocompleteInit,
+					},
+				],
+			}
+		)
+	},
+})
+</script>
+
+<style scoped>
+.row {
+	position: relative;
+	align-items: center;
+}
+.contact .map {
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+}
+.contact {
+	position: relative;
+	min-height: 700px;
+	display: -webkit-box;
+	display: -webkit-flex;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-box-pack: center;
+	-webkit-justify-content: center;
+	-ms-flex-pack: center;
+	justify-content: center;
+	-webkit-box-align: center;
+	-webkit-align-items: center;
+	-ms-flex-align: center;
+	align-items: center;
+	padding: 40px 0;
+}
+.contact .contact-form-card,
+.contact .contact-info-card {
+	padding: 35px;
+	background: #fff;
+	min-height: 300px;
+}
+.contact .contact-title::after {
+	content: '';
+	width: 20px;
+	height: 2px;
+	background: #f85c70;
+	display: block;
+	margin-top: 10px;
+}
+</style>
