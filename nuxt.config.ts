e42b0dab8473ca58534a391c7ca56fa9601c9340
Derek J <derek@derekjohnston.ca>
2024-03-14 16:56:30 -0400

    seo wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: 'Lead Full-Stack Developer - Derek Johnston',
+			meta: [
+				{
+					name: 'description',
+					content:
+						'With a background spanning software development roles at Oiika and E-Data Now!, along with project leadership at YHP, Derek Johnston has gained diverse experience in frontend and backend development. Additionally, his freelance consulting work has provided expertise in web and mobile development. This varied experience has equipped him with a versatile skill set and a deep understanding of software development across different domains.',
+				},
+			],
+			htmlAttrs: {
+				lang: 'en',
+			},
+		},
+	},
 	devtools: { enabled: true },
 	css: [
 		'@fortawesome/fontawesome-svg-core/styles.css',
