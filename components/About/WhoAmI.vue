791ba0d80f5b3502fb012c0e81218fc52295ca45
Derek J <derek@derekjohnston.ca>
2024-07-11 09:22:48 -0400

    use the json

--- components/About/WhoAmI.vue
+++ components/About/WhoAmI.vue
@@ -6,16 +6,22 @@
       span.line
   .card-body
     .row.title
-      h5 A Fullstack / Mobile Developer Located In Our Lovely Earth
+      h5 {{ about.WhoAmI }}
     .row.description.text-justify
-      | With a background spanning software development roles at Oiika and E-Data Now!, along with project leadership at YHP, I've gained diverse experience in frontend and backend development. Additionally, my freelance consulting work has provided expertise in web and mobile development. This varied experience has equipped me with a versatile skill set and a deep understanding of software development across different domains.
+      | {{ about.description }}
       //- | With a diverse background spanning roles in software development, freelance consulting, and project management, I bring a wealth of experience to the table. My journey began as a Software Engineer at Oiika, where I honed my skills in frontend development while contributing to a platform fostering knowledge sharing among tutors and mentors. Subsequently, I transitioned to roles as a Software Developer at E-Data Now!, where I tackled challenges in real-time data collection systems for the automotive industry, and as a Lead Full-Stack Developer at YHP, where I orchestrated projects aligning with strategic goals. Alongside, I ventured into freelance development consulting, offering expertise in web and mobile development. This multifaceted experience has equipped me with a versatile skill set and a deep understanding of the intricacies of software development across various domains.
 </template>
 
 <script>
 // TODO: use json vs static
+import json from '@/assets/about.json'
 export default {
 	name: 'AboutWhoAmIComponent',
+	data() {
+		return {
+			about: json.about,
+		}
+	},
 }
 </script>
 
--- components/Contact/Info.vue
+++ components/Contact/Info.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
 div
-	.row(v-for="(data, index) in info" :key="index")
-		template(v-if="data?.type !== 'Location' || location")
-			.col-4.text-nowrap 
-				b {{data.type}} : &nbsp;
-			.col-8 {{data.data}}
+	.row(v-for="(data, index) in info" :key="index" )
+		.col-4.text-nowrap 
+			b {{data.type}} : &nbsp;
+		.col-8 {{data.data}}
 	Socials
 	.row.py-2
 		.col.text-center
@@ -12,21 +11,16 @@ div
 </template>
 
 <script>
+import about from '~/assets/about.json'
 export default {
 	name: 'ContactInfoComponent',
-	props: {
-		location: {
-			type: Boolean,
-			default: true,
-		},
-	},
 	data() {
 		return {
 			info: [
-				{ type: 'Email', data: 'derek@derekjohnston.ca' },
-				{ type: 'Cell', data: '+1 (905) 926-8279' },
-				{ type: 'Location', data: 'Durham Region, Ontario, Canada' },
-				{ type: 'Open to', data: 'Remote or local work' },
+				{ type: 'Email', data: about.about.email },
+				{ type: 'Cell', data: about.about.cell },
+				{ type: 'Location', data: about.about.location },
+				{ type: 'Open to', data: about.about.lookingFor },
 			],
 		}
 	},
