13d6032164da27624dd6215b67afdf32f0252708
Derek J <derek@derekjohnston.ca>
2024-07-11 09:27:11 -0400

    fix layout and add dynamic location based on users location

--- components/Contact/Index.vue
+++ components/Contact/Index.vue
@@ -1,45 +1,61 @@
 <template lang="pug">
-.row.contact
-	.map 
-		iframe(
-			width="100%"
-			height="100%"
-			style="border: 0"
-			loading="lazy"
-			allowfullscreen
-			referrerpolicy="no-referrer-when-downgrade"
-			src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDpRStcC8FVyAC3QE43Yd4q3m_ucJaQxh8&q=Durham+Region,Ontario")
-	.container
-		.row 
-			.col-lg-8 
-				.contact-form-card 
-					h4.contact-title
-						| Send a message
-					form(action="https://formspree.io/f/derek@derekjohnston.ca" method="post")
-						.form-group
-							input.form-control(type="text" id="name" name="name" placeholder="Name *" required)
-						.form-group 
-							input.form-control(type="email" id="email" name="email" placeholder="Email *" required)
-						.form-group 
-							input.form-control(type="text" id="subject" name="subject" placeholder="Subject *" required)
-						.form-group
-							textarea.form-control(placeholder="Message *" id="message" name="message" rows="7" required)
-						.form-group
-							button.btn.btn-primary(type="submit") 
-								| Send Message 
-			.col-md-12.col-lg-4
-				.contact-info-card 
-					h4.contact-title
-						| Get in touch 
-					Info(:location="false")
+	.row
+		.col-xl-6.p-0
+			.contact
+				.map
+					iframe(
+						width="100%"
+						height="100%"
+						style="border: 0"
+						loading="lazy"
+						allowfullscreen
+						referrerpolicy="no-referrer-when-downgrade"
+						:src="location")
+		.col-xl-6.col-lg-12
+			.row
+				.col-md-6.py-2
+					.contact-form-card.text-center
+						h4.contact-title
+							| Email Me
+						.form-group.py-1
+							input.form-control(type="text" id="name" name="name" value="Derek@DerekJohnston.ca" disabled)
+						form(action="https://formspree.io/f/derek@derekjohnston.ca" method="post")
+							.form-group.py-1
+								input.form-control(type="text" id="name" name="name" placeholder="Name *" required)
+							.form-group.py-1
+								input.form-control(type="email" id="email" name="email" placeholder="Email *" required)
+							.form-group.py-1
+								input.form-control(type="text" id="subject" name="subject" placeholder="Subject *" required)
+							.form-group.py-1
+								textarea.form-control(placeholder="Message *" id="message" name="message" rows="7" required)
+							.form-group.py-1
+								button.btn.btn-primary(type="submit") 
+									| Send Message
+				.col-md-6
+					.contact-info-card.d-sm-none.d-md-block
+						h4.contact-title
+							| Get in touch 
+						Info
+					.d-md-none.py-2
+						Socials
 </template>
 <script>
+//TODO: use json vs static
 import Info from '~/components/Contact/Info'
 
 export default {
 	name: 'ContactComponent',
 	components: {
 		Info,
+	},
+	data() {
+		return {
+			showMap: true,
+			hoveringContact: false,
+			showContact: false,
+			location:
+				'https://www.google.com/maps/embed/v1/place?key=AIzaSyDpRStcC8FVyAC3QE43Yd4q3m_ucJaQxh8&q=Greater+Toronto+Area,Ontario',
+		}
 	},
 	head() {
 		const key = process.env.TOKEN_GOOGLE
@@ -53,6 +69,26 @@ export default {
 				],
 			}
 		)
+	},
+	created() {
+		fetch('https://ipinfo.io/json?token=fd4d7922a87146')
+			.then((response) => response.json())
+			.then((data) => {
+				// Set my location based on where they are
+				// Default is Toronto because everyone knows toronto
+				if (data.country === 'CA' && data.region === 'Ontario') {
+					// They might know newcastle becasue they are located in oshawa
+					if (data.loc === 'Oshawa') {
+						this.location =
+							'https://www.google.com/maps/embed/v1/place?key=AIzaSyDpRStcC8FVyAC3QE43Yd4q3m_ucJaQxh8&q=Newcastle,Ontario'
+					} else {
+						// Otherwise they are they will need to a more broad location
+						this.location =
+							'https://www.google.com/maps/embed/v1/place?key=AIzaSyDpRStcC8FVyAC3QE43Yd4q3m_ucJaQxh8&q=Durham+Region,Ontario'
+					}
+				}
+			})
+			.catch((error) => console.error('Error fetching location:', error))
 	},
 }
 </script>
